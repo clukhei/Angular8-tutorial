1a27fc85885ecf389f0e246c38d3a3136cf2b4e9
Lu Khei <kheiboardwarrior@gmail.com>
2020-07-31 13:32:50 +0800

    angular 8 tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//component decorator, locations for files
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+//component logic
 export class AppComponent {
   title = 'my-app';
 }
